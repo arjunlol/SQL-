f451ff8be391214a711dde224c3785a3f5d355c2
Arjun Lall <lallarjun@gmail.com>
2017-05-22 22:44:16 +0000

    SQL navy database

--- navy_schema.sql
+++ navy_schema.sql
@@ -1,1 +1,38 @@
-SELECT 'hello, PG';
+CREATE TABLE fleets (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+CREATE TABLE ships (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  date_built DATE,
+  fleet_id FOREIGN KEY
+);
+
+CREATE TABLE duties (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+CREATE TABLE sailors (
+  id BIGSERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  dob DATE
+);
+
+CREATE TABLE sailor_duty_ships (
+  id BIGSERIAL PRIMARY KEY,
+  rank VARCHAR(50),
+  start_date DATE,
+  end_date DATE,
+  sailors_id FOREIGN KEY,
+  duties_id FOREIGN KEY,
+  ships_id FOREIGN KEY
+);
+
+INSERT INTO fleets (name) VALUES ('jim' 'john');
+
+
+DROP TABLE IF EXISTS fleets VALUES ('jim' 'john') CASCADE;
+
